8d5cf56faffd60a2cfdada4d0f90fa05f01ed6c9
Ticoxxo <ticoxxo@gmail.com>
2024-02-09 09:05:18 -0700

    Initial Commit

---
+++ ImagesCarrousel/ImagesCarrouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesCarrouselApp.swift
+//  ImagesCarrousel
+//
+//  Created by Alberto Almeida on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagesCarrouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
